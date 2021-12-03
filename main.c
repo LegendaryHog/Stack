c5baa1f65c3dc455d5f60a904e5a74cbd7a41dfd
Kuteinikov Sergey <90862544+LegendaryHog@users.noreply.github.com>
2021-12-03 13:26:00 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,29 +1,58 @@
-#include "Assembler.h"
+#include "stack.h"
+
+void fprint_double (FILE* file, void* ptrdouble);
+void fprint_int (FILE* file, void* ptrint);
+void Stack_Push_d (stack* stk, double push);
+double Stack_Pop_d (stack* stk);
 
 int main (void)
 {
-    size_t buffsize = 0;
+    stack stk = {};
+    stack stk1 = {};
+    Stack_Ctor (&stk1, "int",  sizeof (int), fprint_int);
+    Stack_Ctor (&stk, "double", sizeof(double), fprint_double);
 
-    char* ptrbuff = From_File_to_buffer (&buffsize);
-
-    char* code = Buffer_to_Code (ptrbuff, buffsize);
-
-    FILE* Code = fopen ("machine_code.bin", "wb");
-
-    if (code != NULL)
+    for (int i = 0; i < 20; i++)
     {
-        Disassembler (code);
+        Stack_Push_d (&stk, 10);
     }
-    else
-    {
-        printf ("assembler error\n");
-        return 0;
-    }
+    double a = 0;
+    Stack_Dump (&stk);
 
-    fwrite (code, sizeof (char), sizeof (code), Code);
+    printf ("%lf\n", Stack_Pop_d (&stk));
+    Stack_Dump (&stk);
+    Stack_Dtor (&stk);
 
-    fclose (Code);
-    free (code);
+    int b = 15;
+    Stack_Push (&stk1, &b);
+    Stack_Dump (&stk1);
+    Stack_Pop (&stk1, NULL);
+    Stack_Dump (&stk1);
+    Stack_Dtor (&stk1);
 
     return 0;
 }
+
+void fprint_double (FILE* file, void* ptrdouble)
+{
+    fprintf (file, "%lf", *((double*)ptrdouble));
+}
+void fprint_int (FILE* file, void* ptrint)
+{
+    fprintf (file, "%d", *((int*)ptrint));
+}
+void Stack_Push_d (stack* stk, double push)
+{
+    Stack_Push (stk, &push);
+}
+double Stack_Pop_d (stack* stk)
+{
+    double pop = 0;
+    Stack_Pop (stk, &pop);
+    return pop;
+}
+
+
+
+
+
